7cb2b5cea0e2a3ec7ceb684f2dfd3a3c97608735
amakaseev <aleksey.makaseev@gmail.com>
2024-03-30 19:39:58 +0200

    add grid system

---
+++ Assets/_Project/Scripts/GridSystem/GridObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Match3 {
+
+  public class GridObject<T> {
+    Grid2D<GridObject<T>> grid;
+    int x, y;
+
+    public GridObject(Grid2D<GridObject<T>> grid, int x, int y) {
+      this.grid = grid;
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+}
---
+++ Assets/_Project/Scripts/Match3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Match3 {
+
+  public class Match3: MonoBehaviour {
+    [SerializeField] int width = 8;
+    [SerializeField] int height = 8;
+    [SerializeField] float cellSize = 1.0f;
+    [SerializeField] Vector3 originPosition = Vector3.zero;
+
+    Grid2D<GridObject<Gem>> grid;
+
+    void Start () {
+      grid = Grid2D<GridObject<Gem>>.VerticalGrid(width, height, cellSize, originPosition);
+    }
+
+    private void OnDrawGizmos() {
+      grid?.DebugDraw();
+    }
+  }
+
+}
